1b995497cddae970c179e347ea4b48696030fb87
VasyaXXI <31704859+VasyaXXI@users.noreply.github.com>
2017-12-19 15:26:46 +0300

    Update turing.c

--- turing.c
+++ turing.c
@@ -2,9 +2,11 @@
 #include "stdlib.h"
 #include "string.h"
 
-int size = 1;
-int n = 0;
+int size;
+int n;
 int *tape;
+size = 1;
+n = 0;
 
 int movl(void)
 {
@@ -16,7 +18,7 @@ int movl(void)
         m = (int*) realloc (tape, sizeof(int) * size);
         if(m != NULL)
         {
-            for (i = size- 1; i != 0; i--)
+            for (i = size-1; i != 0; i--)
                 m[i]=m[i-1];
             m[0] = 0;
             tape = m;
@@ -30,7 +32,7 @@ int movl(void)
 }
 int movr(void)
 {
-    if (n == (size - 1))
+    if (n == (size-1))
     {
         int* m;
         size++;
@@ -50,7 +52,7 @@ int movr(void)
 }
 int inc (void)
 {
-    if(tape[n] < 255)
+    if(tape[n]<255)
         tape[n]++;
     else
         tape[n] = 0;
@@ -58,7 +60,7 @@ int inc (void)
 }
 int dec (void)
 {
-    if(tape[n] > 0)
+    if(tape[n]>0)
         tape[n]--;
     else
         tape[n] = 255;
@@ -66,35 +68,42 @@ int dec (void)
 }
 int orders (char** a, int f, int l)
 {
-    int i, m = 0;
-    for (i = f; i < l; i++)
+    int i, m;
+    m = 0;
+    for (i = f; i<l; i++)
     {
         if (strcmp(a[i], "movl") == 0)
             movl();
-        else if (strcmp(a[i], "movr") == 0)
-            movr();
-        else if (strcmp(a[i], "inc") == 0)
-            inc();
-        else if (strcmp(a[i], "dec") == 0)
-            dec();
-        else if (strcmp(a[i], "print") == 0)
-            printf("\n%d", tape[n]);
-        else if (strcmp(a[i], "get") == 0)
-        {
-            printf("\nEnter symbol:");
-            tape[n] = getchar();
-        }
-        else if (strcmp(a[i], "printc") == 0)
-            printf("\n%c", tape[n]);
-        else if (strcmp(a[i], "begin") == 0)
-             m=i+1;
-        else if (strcmp(a[i], "end") == 0)
-            {
-                while (tape[n] != 0)
-                    orders(a, m, i);
-            }
         else
-            printf("\nERROR");
+            if (strcmp(a[i], "movr") == 0)
+                movr();
+            else
+                if (strcmp(a[i], "inc") == 0)
+                    inc();
+                else
+                    if (strcmp(a[i], "dec") == 0)
+                        dec();
+                    else
+                        if (strcmp(a[i], "print") == 0)
+                            printf("\n%d", tape[n]);
+                        else
+                            if (strcmp(a[i], "get") == 0)
+                            {
+                                printf("\nEnter symbol:");
+                                tape[n] = getchar();
+                            }
+                            else
+                                if (strcmp(a[i], "printc") == 0)
+                                    printf("\n%c", tape[n]);
+                                else
+                                    if (strcmp(a[i], "begin") == 0)
+                                        m = i+1;
+                                    else
+                                        if (strcmp(a[i], "end") == 0)
+                                        {
+                                            while (tape[n] != 0)
+                                            orders(a, m, i);
+                                        }
     }
     return 0;
 }
@@ -107,7 +116,7 @@ char* space (char* s, char* a)
     {
         if (s[i] != ' ' && s[i] != '\n')
         {
-            a[j]=s[i];
+            a[j] = s[i];
             j++;
             a = (char*) realloc (a,(j+6)*sizeof(char));
         }
@@ -120,17 +129,15 @@ int main()
 {
     char s[255];
     char **a;
-    int num = 0, i = 0;;
+    int num, i;
+    num = 0;
+    i = 0;
     char *fname = NULL;
-    FILE * file;
+    FILE *file;
     file = fopen("file.txt", "r");
-    while (file == NULL)
-    {
-        printf(" Could not open file");
-        scanf("%s", fname);
-        file=fopen(fname, "rb");
-    }
-    a=(char **) malloc ((num+1)*sizeof(char *));
+    if (file == NULL)
+        printf(" Could not open file\n");
+    a = (char **) malloc ((num+1)*sizeof(char *));
     while(fgets(s, 255, file))
     {
         if(s[0] != '*')
@@ -142,7 +149,7 @@ int main()
     }
     fclose(file);
     tape = (int*) malloc (sizeof(int));
-    tape[n]=0;
+    tape[n] = 0;
     orders(a, 0, num);
     for(i = 0; i < num; i++)
         free(a[num]);
